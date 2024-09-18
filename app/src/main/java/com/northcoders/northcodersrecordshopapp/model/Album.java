c3540adc165ef7cfde01657b5da2104f488ea866
ag-af <agabialves11@gmail.com>
2024-09-18 15:27:48 +0100

    feat: Implement Data Binding in the Model.

--- app/src/main/java/com/northcoders/northcodersrecordshopapp/model/Album.java
+++ app/src/main/java/com/northcoders/northcodersrecordshopapp/model/Album.java
@@ -1,8 +1,12 @@
 package com.northcoders.northcodersrecordshopapp.model;
 
-import com.google.gson.annotations.SerializedName;
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
 
-public class Album {
+import com.google.gson.annotations.SerializedName;
+import com.northcoders.northcodersrecordshopapp.BR;
+
+public class Album extends BaseObservable {
     @SerializedName("id")
     private long id;
     @SerializedName("title")
@@ -30,59 +34,73 @@ public class Album {
 
     public Album() {}
 
+    @Bindable
     public long getId() {
         return id;
     }
 
     public void setId(long id) {
         this.id = id;
+        notifyPropertyChanged(BR.id);
     }
 
+    @Bindable
     public String getTitle() {
         return title;
     }
 
     public void setTitle(String title) {
         this.title = title;
+        notifyPropertyChanged(BR.title);
     }
 
+    @Bindable
     public String getArtist() {
         return artist;
     }
 
     public void setArtist(String artist) {
         this.artist = artist;
+        notifyPropertyChanged(BR.artist);
     }
 
+    @Bindable
     public Genre getGenre() {
         return genre;
     }
 
     public void setGenre(Genre genre) {
         this.genre = genre;
+        notifyPropertyChanged(BR.genre);
     }
 
+    @Bindable
     public int getReleaseYear() {
         return releaseYear;
     }
 
     public void setReleaseYear(int releaseYear) {
         this.releaseYear = releaseYear;
+        notifyPropertyChanged(BR.releaseYear);
     }
 
+    @Bindable
     public double getPrice() {
         return price;
     }
 
     public void setPrice(double price) {
         this.price = price;
+        notifyPropertyChanged(BR.price);
     }
 
+    @Bindable
     public int getStock() {
         return stock;
     }
 
     public void setStock(int stock) {
         this.stock = stock;
+        notifyPropertyChanged(BR.stock);
     }
 }
