709ecf6a147b522e24aa430566ac8df173bfb21a
ag-af <agabialves11@gmail.com>
2024-09-18 12:13:17 +0100

    feat: Create a Retrofit Instance and AlbumApiService.

---
+++ app/src/main/java/com/northcoders/northcodersrecordshopapp/service/RetrofitInstance.java
@@ -0,0 +1,31 @@
+package com.northcoders.northcodersrecordshopapp.service;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitInstance {
+
+    private static Retrofit retrofit = null;
+    private static final String BASE_URL = "http://recordshop-env.eba-3jmedaqh.eu-west-2.elasticbeanstalk.com/api/v1/";
+
+    public static AlbumApiService getService() {
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build();
+
+        if (retrofit == null) {
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(okHttpClient)
+                    .build();
+        }
+
+        return retrofit.create(AlbumApiService.class);
+    }
+}
