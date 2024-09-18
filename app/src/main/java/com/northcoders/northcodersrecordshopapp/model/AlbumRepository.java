0cd300356546ad96bf39544c48e2100ba3457960
ag-af <agabialves11@gmail.com>
2024-09-18 12:21:05 +0100

    feat: Create Repository layer.

---
+++ app/src/main/java/com/northcoders/northcodersrecordshopapp/model/AlbumRepository.java
@@ -0,0 +1,50 @@
+package com.northcoders.northcodersrecordshopapp.model;
+
+import android.app.Application;
+import android.util.Log;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.northcoders.northcodersrecordshopapp.service.AlbumApiService;
+import com.northcoders.northcodersrecordshopapp.service.RetrofitInstance;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlbumRepository {
+
+    private List<Album> albums = new ArrayList<>();
+    private MutableLiveData<List<Album>> mutableLiveData = new MutableLiveData<>();
+    private Application application;
+
+    public AlbumRepository(Application application) {
+        this.application = application;
+    }
+
+    public MutableLiveData<List<Album>> getMutableLiveData() {
+        AlbumApiService albumApiService = RetrofitInstance.getService();
+        Call<List<Album>> call = albumApiService.getAllAlbums();
+
+        call.enqueue(new Callback<List<Album>>() {
+
+            @Override
+            public void onResponse(Call<List<Album>> call, Response<List<Album>> response) {
+                if (response.isSuccessful() & response.body() != null) {
+                    List<Album> albumList = response.body();
+                    mutableLiveData.setValue(albumList);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Album>> call, Throwable throwable) {
+                Log.i("HTTP Failure", throwable.getMessage());
+
+            }
+        });
+        return mutableLiveData;
+    }
+}
