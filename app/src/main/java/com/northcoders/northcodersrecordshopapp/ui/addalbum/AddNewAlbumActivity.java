893adea0e201acaf7ac8028dd480409893350838
ag-af <agabialves11@gmail.com>
2024-09-19 12:23:56 +0100

    feat: Add click handlers in the AddNewAlbumActivity.

--- app/src/main/java/com/northcoders/northcodersrecordshopapp/ui/addalbum/AddNewAlbumActivity.java
+++ app/src/main/java/com/northcoders/northcodersrecordshopapp/ui/addalbum/AddNewAlbumActivity.java
@@ -8,20 +8,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.northcoders.northcodersrecordshopapp.R;
+import com.northcoders.northcodersrecordshopapp.databinding.ActivityAddNewAlbumBinding;
+import com.northcoders.northcodersrecordshopapp.model.Album;
+import com.northcoders.northcodersrecordshopapp.ui.mainactivity.MainActivityViewModel;
 
 public class AddNewAlbumActivity extends AppCompatActivity {
+
+    private ActivityAddNewAlbumBinding binding;
+    private AddAlbumClickHandlers handlers;
+    private Album album;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_add_new_album);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        album = new Album();
+
+        binding = DataBindingUtil.setContentView(this,
+                R.layout.activity_add_new_album);
+
+        MainActivityViewModel viewModel = new ViewModelProvider(this)
+                .get(MainActivityViewModel.class);
+
+        handlers = new AddAlbumClickHandlers(this, viewModel);
+
+        binding.setAlbum(album);
+        binding.setClickHandler(handlers);
     }
 }
